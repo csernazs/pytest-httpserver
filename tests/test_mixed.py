5195d4256a50716716adc5aaaeb2ea6800dd086a
Cserna Zsolt <cserna.zsolt@gmail.com>
2018-08-06 22:01:36 +0200

    tests/test_mixed.py: time to test all types of expectations together

---
+++ tests/test_mixed.py
@@ -0,0 +1,83 @@
+import pytest
+import requests
+from pytest_httpserver.httpserver import HTTPServer
+
+
+def _setup_oneshot(server: HTTPServer):
+    server.expect_request("/generic").respond_with_data("OK generic")
+    server.expect_oneshot_request("/oneshot1").respond_with_data("OK oneshot1")
+    server.expect_oneshot_request("/oneshot2").respond_with_data("OK oneshot2")
+
+
+def _setup_ordered(server: HTTPServer):
+    server.expect_oneshot_request("/ordered1", ordered=True).respond_with_data("OK ordered1")
+    server.expect_oneshot_request("/ordered2", ordered=True).respond_with_data("OK ordered2")
+
+
+def _setup_all(server: HTTPServer):
+    _setup_oneshot(server)
+    _setup_ordered(server)
+
+
+def test_oneshot_and_generic_happy_path1(httpserver: HTTPServer):
+    _setup_oneshot(httpserver)
+    assert requests.get(httpserver.url_for("/generic")).text == "OK generic"
+    assert requests.get(httpserver.url_for("/oneshot1")).text == "OK oneshot1"
+    assert requests.get(httpserver.url_for("/oneshot2")).text == "OK oneshot2"
+    assert requests.get(httpserver.url_for("/generic")).text == "OK generic"
+
+    assert len(httpserver.oneshot_handlers) == 0
+
+
+def test_oneshot_and_generic_happy_path2(httpserver: HTTPServer):
+    _setup_oneshot(httpserver)
+    assert requests.get(httpserver.url_for("/generic")).text == "OK generic"
+    assert requests.get(httpserver.url_for("/oneshot2")).text == "OK oneshot2"
+    assert requests.get(httpserver.url_for("/oneshot1")).text == "OK oneshot1"
+    assert requests.get(httpserver.url_for("/generic")).text == "OK generic"
+
+    assert len(httpserver.oneshot_handlers) == 0
+
+
+def test_all_happy_path1(httpserver: HTTPServer):
+    _setup_all(httpserver)
+
+    # ordered must go first
+    assert requests.get(httpserver.url_for("/ordered1")).text == "OK ordered1"
+    assert requests.get(httpserver.url_for("/ordered2")).text == "OK ordered2"
+    assert requests.get(httpserver.url_for("/generic")).text == "OK generic"
+    assert requests.get(httpserver.url_for("/oneshot2")).text == "OK oneshot2"
+    assert requests.get(httpserver.url_for("/oneshot1")).text == "OK oneshot1"
+    assert requests.get(httpserver.url_for("/generic")).text == "OK generic"
+
+    assert len(httpserver.oneshot_handlers) == 0
+    assert len(httpserver.ordered_handlers) == 0
+
+
+def test_all_ordered_missing(httpserver: HTTPServer):
+    _setup_all(httpserver)
+
+    # ordered is missing so everything must fail
+    # a.k.a. permanently fail
+
+    requests.get(httpserver.url_for("/generic"))
+    with pytest.raises(AssertionError):
+        httpserver.check_assertions()
+
+    requests.get(httpserver.url_for("/oneshot2"))
+    with pytest.raises(AssertionError):
+        httpserver.check_assertions()
+
+    requests.get(httpserver.url_for("/oneshot1"))
+    with pytest.raises(AssertionError):
+        httpserver.check_assertions()
+
+    requests.get(httpserver.url_for("/generic"))
+    with pytest.raises(AssertionError):
+        httpserver.check_assertions()
+
+    # handlers must be still intact but as the ordered are failed
+    # everything will fail
+    assert len(httpserver.ordered_handlers) == 2
+    assert len(httpserver.oneshot_handlers) == 2
+    assert len(httpserver.handlers) == 1
