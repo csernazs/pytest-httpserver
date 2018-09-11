1b09a2a53ab85effe8c459c81da8eb763ced2c30
Cserna Zsolt <cserna.zsolt@gmail.com>
2018-09-11 08:17:10 +0200

    test_port_changing: add more tests not requiring environment
    Do the best what we can when environment is not set.

--- tests/test_port_changing.py
+++ tests/test_port_changing.py
@@ -2,9 +2,28 @@
 import os
 import pytest
 from pytest_httpserver import HTTPServer
+from pytest_httpserver.pytest_plugin import get_httpserver_listen_address
 
 PORT_KEY = "PYTEST_HTTPSERVER_PORT"
 HOST_KEY = "PYTEST_HTTPSERVER_HOST"
+
+
+@pytest.fixture
+def tmpenv():
+    old_vars = {}
+    for key in (HOST_KEY, PORT_KEY):
+        old_vars[key] = os.environ.get(key)
+
+    os.environ[HOST_KEY] = "5.5.5.5"
+    os.environ[PORT_KEY] = "12345"
+
+    yield
+
+    for key, value in old_vars.items():
+        if value:
+            os.environ[key] = value
+        else:
+            del os.environ[key]
 
 
 @pytest.mark.skipif(HOST_KEY not in os.environ,
@@ -17,3 +36,9 @@ def test_host_changing_by_environment(httpserver: HTTPServer):
                     reason="requires {} environment variable".format(PORT_KEY))
 def test_port_changing_by_environment(httpserver: HTTPServer):
     assert httpserver.port == int(os.environ[PORT_KEY])
+
+
+def test_get_httpserver_listen_address_with_env(tmpenv):
+    address = get_httpserver_listen_address()
+    assert address[0] == "5.5.5.5"
+    assert address[1] == 12345
