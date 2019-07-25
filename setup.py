e3a12071a7fef67b13e077ed5773b68f1674ae4d
Cserna Zsolt <cserna.zsolt@gmail.com>
2019-07-25 08:34:32 +0200

    setup.py: bump version to 3.0.1
    Also update sphinx documentation.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = open("README.md").read()
 
 setup(
     name="pytest_httpserver",
-    version="0.3.0",
+    version="0.3.1",
     url="https://www.github.com/csernazs/pytest-httpserver",
     packages=find_packages(),
     author="Zsolt Cserna",
