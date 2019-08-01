ec1811ec45322fb2e7c8cd0d02c8776aae0a9456
Cserna Zsolt <zsolt@balabit.com>
2019-08-01 14:31:40 +0200

    setup.py: version bump to 0.3.3
    Bump version to 0.3.3 in setup.py and sphinx.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = open("README.md").read()
 
 setup(
     name="pytest_httpserver",
-    version="0.3.2",
+    version="0.3.3",
     url="https://www.github.com/csernazs/pytest-httpserver",
     packages=find_packages(),
     author="Zsolt Cserna",
