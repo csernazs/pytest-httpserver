882fe39c67ef70d091f3babbec98fbe2e8604ca1
Zsolt Cserna <cserna.zsolt@gmail.com>
2020-08-03 21:29:20 +0200

    httpserver: add URI matching
    Extend URI matching functionality which now accepts:
    
    * a URIPattern object, whose match() method will be called
    * a compiled regexp returned by re.compile(). In this case the
      URI will be matched against the regexp.
    
    In both cases the URI will be an absolute path starting with a slash.
    
    Fixes #34 and #36.

--- pytest_httpserver/__init__.py
+++ pytest_httpserver/__init__.py
@@ -8,4 +8,4 @@ This is package provides the main API for the pytest_httpserver package.
 from .httpserver import HTTPServer
 from .httpserver import HTTPServerError, Error, NoHandlerError
 from .httpserver import WaitingSettings, HeaderValueMatcher, RequestHandler
-from .httpserver import URI_DEFAULT, METHOD_ALL
+from .httpserver import URIPattern, URI_DEFAULT, METHOD_ALL
