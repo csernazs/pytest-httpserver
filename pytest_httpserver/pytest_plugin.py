d3930324730d9dd737893f5d877dd13d2bc5131c
Mohamed Khalifa <mohamed.khalifa@rwth-aachen.de>
2021-02-10 16:58:45 +0100

    Replace PluginHTTPServer with a session scoped fixture

--- pytest_httpserver/pytest_plugin.py
+++ pytest_httpserver/pytest_plugin.py
@@ -1,9 +1,7 @@
-
 import os
 import warnings
 
 import pytest
-
 from .httpserver import HTTPServer
 
 
@@ -27,30 +25,28 @@ def get_httpserver_listen_address():
     if listen_port:
         listen_port = int(listen_port)
 
-    return (listen_host, listen_port)
+    return listen_host, listen_port
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def httpserver_listen_address():
     return get_httpserver_listen_address()
 
 
-@pytest.fixture
-def httpserver(httpserver_listen_address):
-    if Plugin.SERVER:
-        Plugin.SERVER.clear()
-        yield Plugin.SERVER
-        return
-
+@pytest.fixture(scope="session")
+def _httpserver(httpserver_listen_address):
     host, port = httpserver_listen_address
     if not host:
         host = HTTPServer.DEFAULT_LISTEN_HOST
     if not port:
         port = HTTPServer.DEFAULT_LISTEN_PORT
 
-    server = PluginHTTPServer(host=host, port=port)
+    server = HTTPServer(host=host, port=port)
     server.start()
     yield server
+    server.clear()
+    if server.is_running():
+        server.stop()
 
 
 def pytest_sessionfinish(session, exitstatus):  # pylint: disable=unused-argument
@@ -69,3 +65,9 @@ def pytest_fixture_setup(fixturedef, request):  # pylint: disable=unused-argumen
         yield
     else:
         yield
+
+
+@pytest.fixture
+def httpserver(_httpserver):
+    yield _httpserver
+    _httpserver.clear()
