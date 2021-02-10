2271c72bba87e728c17253d2f9126b71111e0ae4
Mohamed Khalifa <mohamed.khalifa@rwth-aachen.de>
2021-02-10 16:58:45 +0100

    Replace PluginHTTPServer with a session scoped fixture

--- pytest_httpserver/pytest_plugin.py
+++ pytest_httpserver/pytest_plugin.py
@@ -1,23 +1,7 @@
-
-
 import os
 
 import pytest
-from .httpserver import HTTPServer
-
-
-class Plugin:
-    SERVER = None
-
-
-class PluginHTTPServer(HTTPServer):
-    def start(self):
-        super().start()
-        Plugin.SERVER = self
-
-    def stop(self):
-        super().stop()
-        Plugin.SERVER = None
+from pytest_httpserver import HTTPServer
 
 
 def get_httpserver_listen_address():
@@ -26,34 +10,31 @@ def get_httpserver_listen_address():
     if listen_port:
         listen_port = int(listen_port)
 
-    return (listen_host, listen_port)
+    return listen_host, listen_port
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def httpserver_listen_address():
     return get_httpserver_listen_address()
 
 
-@pytest.fixture
-def httpserver(httpserver_listen_address):
-    if Plugin.SERVER:
-        Plugin.SERVER.clear()
-        yield Plugin.SERVER
-        return
-
+@pytest.fixture(scope="session")
+def _httpserver(httpserver_listen_address):
     host, port = httpserver_listen_address
     if not host:
         host = HTTPServer.DEFAULT_LISTEN_HOST
     if not port:
         port = HTTPServer.DEFAULT_LISTEN_PORT
 
-    server = PluginHTTPServer(host=host, port=port)
+    server = HTTPServer(host=host, port=port)
     server.start()
     yield server
+    server.clear()
+    if server.is_running():
+        server.stop()
 
 
-def pytest_sessionfinish(session, exitstatus):  # pylint: disable=unused-argument
-    if Plugin.SERVER is not None:
-        Plugin.SERVER.clear()
-        if Plugin.SERVER.is_running():
-            Plugin.SERVER.stop()
+@pytest.fixture
+def httpserver(_httpserver):
+    yield _httpserver
+    _httpserver.clear()
