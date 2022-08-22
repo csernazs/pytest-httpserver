058dddb6e834b4f710659ad06ee8ebef388fab97
Cserna Zsolt <cserna.zsolt@gmail.com>
2022-08-22 13:45:52 +0300

    test_release.py: add test to check wheel and sdist contents
    The main purpose of this test is to ensure that whell and sdist are having
    the contents we expect, and no other contents. Partially, this is to avoid
    issues described in #154.
    
    It also checks classifiers that they are in-sync with the python version
    described in the toml.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,15 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--ssl", action="store_true", default=False, help="run ssl tests")
+    parser.addoption("--release", action="store_true", default=False, help="run release tests")
 
 
 def pytest_runtest_setup(item):
     markers = [marker.name for marker in item.iter_markers()]
+
     if not item.config.getoption("--ssl") and "ssl" in markers:
         pytest.skip()
     if item.config.getoption("--ssl") and "ssl" not in markers:
+        pytest.skip()
+    if not item.config.getoption("--release") and "release" in markers:
         pytest.skip()
