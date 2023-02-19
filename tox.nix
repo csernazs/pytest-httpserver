ddb70d05a1315af4eca1ae17487bf6b293c501fa
Cserna Zsolt <cserna.zsolt@gmail.com>
2023-02-19 09:58:14 +0100

    fix tox, add tox.nix

---
+++ tox.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+  toxPython = p: p.withPackages ( p: [ p.pip ] );
+  basePython = p: (p.withPackages ( p: [ p.virtualenv p.pip p.tox ] ));
+
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    (basePython python311)
+    (toxPython python310)
+    (toxPython python39)
+    (toxPython python38)
+    (toxPython python37)
+
+    bashInteractive
+  ];
+}
