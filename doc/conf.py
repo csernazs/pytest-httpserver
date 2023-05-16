a8595ff142b0189b6028a0cc4f6038bcd176fdf6
Cserna Zsolt <cserna.zsolt@gmail.com>
2023-05-16 21:27:30 +0200

    Version bump to 1.0.7

--- doc/conf.py
+++ doc/conf.py
@@ -66,7 +66,7 @@ author = "Zsolt Cserna"
 # built documents.
 #
 # The short X.Y version.
-version = "1.0.6"
+version = "1.0.7"
 # The full version, including alpha/beta/rc tags.
 release = version
 
